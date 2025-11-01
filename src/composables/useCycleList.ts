00f84c1666d1b8c88976980d398b0d91f14e1f6f
Francisco Diosdado <drz.y2k@gmail.com>
2025-10-31 21:15:06 -0600

    Return Data and Functions From Composables

--- src/composables/useCycleList.ts
+++ src/composables/useCycleList.ts
@@ -1,4 +1,19 @@
+import { ref } from 'vue';
+
 //los argumentos se reciben igual que una funcion normal
+
 export const useCycleList = (list: any[]) => {
 
+	// composables simples a veces retornan directamente un valor sin que sea objeto
+	// return ref('');
+
+	//los composables retornan por lo general un objeto
+	//donde tenemos las variables reactivas y funciones
+	//y podemos usar la api de vue
+	return {
+		state: ref(''),
+		prev: () => {},
+		next: () => {},
+		go: () => {},
+	};
 };
