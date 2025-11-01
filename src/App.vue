582fef3f64e154dfd867bbd392d863b0ac462eac
Francisco Diosdado <drz.y2k@gmail.com>
2025-10-31 21:32:55 -0600

    Define Reactive State and Functions within a Composable

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+//importamos nuestro composable
+
+import { useCycleList } from './composables/useCycleList';
+
+//inicializamos nuestro composable con sus funciones y su estado
+
+const { state, next, prev } = useCycleList([
+	'Dog',
+	'Cat',
+	'Mouse',
+	'Hamster',
+	'Parrot',
+]);
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<div>
+		<!-- utilizamos el estado y llamamos las funciones para, ver su funcionalidad -->
+		{{ state }}
+		<button @click="prev">previous</button>
+		<button @click="next">next</button>
+	</div>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+	transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+	filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+	filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+
--- src/composables/useCycleList.ts
+++ src/composables/useCycleList.ts
@@ -1,8 +1,18 @@
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 //los argumentos se reciben igual que una funcion normal
 
 export const useCycleList = (list: any[]) => {
+
+	//creamos las refs y computed que nos ayudaran a manejar el estado
+	//dentro de nuestro composable
+
+	const activeIndex = ref(0);
+
+	//el state simplemente retornará el elemento activo con el indice actual
+	//del arreglo que mandamos como argumento
+
+	const state = computed(() => list[activeIndex.value]);
 
 	// composables simples a veces retornan directamente un valor sin que sea objeto
 	// return ref('');
@@ -10,10 +20,32 @@ export const useCycleList = (list: any[]) => {
 	//los composables retornan por lo general un objeto
 	//donde tenemos las variables reactivas y funciones
 	//y podemos usar la api de vue
+
+	//creamos las funciones next y prev, que modifican
+	//el valor del indice en base a su valor para ciclar
+	//nuestro arreglo
+
+	function next() {
+		if (activeIndex.value == list.length - 1) {
+			activeIndex.value = 0;
+		} else {
+			activeIndex.value++;
+		}
+	}
+
+	function prev() {
+		if (activeIndex.value == 0) {
+			activeIndex.value = list.length - 1;
+		} else {
+			activeIndex.value--;
+		}
+	}
+
+	//exportamos a modo de objeto
 	return {
-		state: ref(''),
-		prev: () => {},
-		next: () => {},
+		state,
+		prev,
+		next,
 		go: () => {},
 	};
 };
